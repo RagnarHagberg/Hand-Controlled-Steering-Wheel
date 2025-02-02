779174cebec4df24aa212e5e17ac288c608809f5
Ragnar <Ragnarhagberg@gmail.com>
2025-02-02 19:47:13 +0100

    Fine tune car and add a track

--- Godot/SteeringWheelProject/Scenes/Car/car.gd
+++ Godot/SteeringWheelProject/Scenes/Car/car.gd
@@ -1,13 +1,51 @@
 extends VehicleBody3D
 
-var horsepower = 100
-
+var horsepower = 3000
+var target_speed = 10
+var current_effect = 500
+var coin_count = 0
+# P = f * v
+# f = P/v
 var steer_limit = deg_to_rad(30)
 
 var input_steering = 0
 func _physics_process(delta):
-	engine_force = horsepower
-
+	engine_force = calculate_engine_force()
+	
+	# Correct rotation if it falls over
+	if rotation_degrees.z < -30 or rotation_degrees.z > 30:
+		rotation_degrees.z = 0
+		
+	print("engine_force" + str(engine_force))
+	print("linear velocity" + str(linear_velocity))
+	print("total velocity" + str(get_velocity()))
 func set_input_steering(new_value):
 	input_steering = new_value
+	
 	steering = min(input_steering, steer_limit)
+
+func calculate_engine_force():
+	
+	
+	var v = get_velocity()
+	
+	if v < target_speed:
+		current_effect += 10
+	else:
+		current_effect -= 10
+		
+	
+	var P = min(current_effect, horsepower)
+	var f = P/v
+	return f
+func get_velocity():
+	var total_velocity = ((linear_velocity[0]**2)+(linear_velocity[1]**2)+(linear_velocity[2]**2))**0.5
+	
+	return total_velocity
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.get_parent().is_in_group("Coin"):
+		area.get_parent().queue_free()
+		coin_count += 1
+		get_parent().set_coins(coin_count)
--- Godot/SteeringWheelProject/Scenes/Game/game.gd
+++ Godot/SteeringWheelProject/Scenes/Game/game.gd
@@ -7,4 +7,7 @@ func _on_server_rotation_changed(rotation_data) -> void:
 	rotation_radians = (rotation_data["rotation_angle"])
 	var steering_wheel_rotation = -(rotation_radians - 3.14/2)
 	get_node("Car/Camera3D/Wheel").rotation.x = (steering_wheel_rotation)
-	get_node("Car").set_input_steering(steering_wheel_rotation/5)
+	get_node("Car").set_input_steering(steering_wheel_rotation/10)
+
+func set_coins(new_value):
+	get_node("Hud/Label").text = "Coins: " + str(new_value)
